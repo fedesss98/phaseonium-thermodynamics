65e86ac2287488d8541d524cecacb952de5f58cd
Federico Amato <federico.amato01@unipa.it>
2026-01-15 15:23:59 +0100

    New export and import

--- julia/src/Phaseonium.jl
+++ julia/src/Phaseonium.jl
@@ -1,12 +1,13 @@
 
 module Phaseonium
 
-export Cavity, StrokeState, kraus_operators, create, destroy, thermalization_stroke, temperature
+export Cavity, StrokeState, kraus_operators, create, destroy, thermalization_stroke, adiabatic_stroke, temperature
 public bosonic_operators
 
 using DifferentialEquations
 using LinearAlgebra
 using SparseArrays
+using Serialization
 
 include("./Measurements.jl")
 include("./OpticalCavity.jl")
