9daa3fae2c6d4d1bd7894ab9180910ed10d9df41
Federico Amato <federico.amato01@unipa.it>
2026-01-21 08:15:42 +0100

    Update suggested workflow

--- julia/scripts/run_cycle.jl
+++ julia/scripts/run_cycle.jl
@@ -1,4 +1,3 @@
-using Serialization: serialize_array_data
 """
 Suggested workflow:
 
@@ -7,52 +6,25 @@ Created in init.jl and globally available:
 `evolution`::StrokeState = struct created in initialization with initial thermal ρ and cavity;
 `cavity`::Cavity = cavity struct containing all the cavity settings
 
-Run the evolution of the cavity state step by step throughout the cycle:
-## Cycle 0-1
-`ρ`, `ρ_evolution` = `thermalize_by_phaseonium("heating", cavity, config, ρ0=evolution.ρ)`
-### Options
- - use `load=true` to load the thermalized state from the path "data/stepbystep_evolution/state_1_thermalized.jl"
- - use `verbose=true` to plot the temperature evolution and save it in "img" folder.
-
-## Save evolution
-This will save a StrokeState with all the history of the evolution 
-and the final states of the cavity and the field.
-`cavity_evolution` = `[cavity.length for _ in 1:length(ρ_evolution)]`
-`update_evolution!(evolution, ρ_evolution, cavity_evolution)`
-`save_evolution(evolution)`
-Run the following block to execute the whole workflow
+Run the evolution of the cavity state step by step throughout the cycle
+(choose the `stroke_function` accordingly).
 `
 begin
-r, revo = thermalize_by_phaseonium("heating", cavity, config, ρ0=evolution.ρ)
-cevo = [cavity.length for _ in 1:length(revo)]
-time = collect(range(config.Δt, config.Δt*config.time.isochore, length=config.samplings.isochore))
+step = 2
+process = "expansion"
+revo, cevo, time = stroke_function(process,cavity,config,ρ0=evolution.ρ,verbose=true);
 update_evolution!(evolution, revo, cevo, time)
-save_evolution(evolution, 1)
+save_evolution(evolution, step)
 end
 `
 
----
-# Cycle 1-2
-Run the following block to evolve the cavity and save the evolution
-`
-begin
-time, cevo = move_by_pressure("expansion", cavity,config,ρ=evolution.ρ,verbose=true);
-revo = [evolution.ρ for _ in 1:length(cevo)]
-time=[evolution.time[end]+t for t in time]
-update_evolution!(evolution, revo, cevo, time)
-save_evolution(evolution, 2)
-end
-`
+All the cycle can be automated with the `cycle` function.
 
----
-# Cycle 2-3
-Cool down the cavity with phaseonium atoms.
-Run the following block to evolve the cavity and save the evolution
 `
 begin
-revo, cevo, time = thermalize_by_phaseonium("cooling",cavity,config,ρ0=evolution.ρ,verbose=true);
-update_evolution!(evolution, revo, cevo, time)
-save_evolution(evolution, 2)
+n_cycles=5
+evolution = cycle(config, n_cycles, cavity=evolution.c₁, ρ0=evolution.ρ, verbose=true)
+println("\nSIMULATION COMPLETED")
 end
 `
 """
